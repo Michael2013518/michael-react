95f5df8c7695caa53cbefdd9b25f2eb11da48a9c
michael <zdong2010@gmail.com>
2019-10-31 16:02:19 +0800

    dom diff sample of react

---
+++ src/c05/DomDiff.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default () => (
+    <div style={{ fontSize: "14px", width: "100%", textAlign:"left", paddingTop:"20px"}}>
+    <a href="https://supnate.github.io/react-dom-diff/index.html" rel="noopener noreferrer"  target="_blank">
+      https://supnate.github.io/react-dom-diff/index.html
+    </a>
+    </div>
+)
